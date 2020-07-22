a322fb4d7db114211c60e2effb19bd327e9d27d1
Tom Moya Schau <moya@asofterspace.com>
2020-07-22 23:49:03 +0200

    Enable showing cards, which is kind of helpful when one tries to create a card game

--- server/gameSelector.js
+++ server/gameSelector.js
@@ -1,5 +1,29 @@
 window.startGame = function(game) {
 
+	window.gameFolder = "/games/" + game + "/";
+
 	document.getElementById("intro").style.display = "none";
 	document.getElementById("gameSelection").style.display = "none";
+
+	// TODO :: get client height of the window
+	windowClientHeight = 800;
+	gameAreaHeight = windowClientHeight - document.getElementById("nameBar").clientHeight;
+	document.getElementById("gameArea").style.height = gameAreaHeight + "px";
+
+	switch (game) {
+		case 'elfik':
+			startElfik();
+			break;
+	}
+}
+
+startElfik = function() {
+
+	loadCard(gameFolder + "characters/" + "cendrerz3.jpg", 0.5, 0.5);
+}
+
+loadCard = function(imgPath, x, y) {
+
+	// TODO :: draw the image on the canvas with a certain size and positioned so that the center of the image is
+	// at the (x,y) position of the visible board game area, where x and y are on a scale from 0 to 1
 }
